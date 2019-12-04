b34483247ebbe947d86664ca23560f6cf5ab406b
Ján Paraska <jparaska65@gmail.com>
2019-12-04 19:12:02 +0100

    When to use a game instance

---
+++ Source/S01_PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance() {
+	UE_LOG(LogTemp, Warning, TEXT("Constructor of game instance"));
+}
+
+void UPuzzlePlatformsGameInstance::Init() {
+	UE_LOG(LogTemp, Warning, TEXT("Init function of game instance"));
+}
